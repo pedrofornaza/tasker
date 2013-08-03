e517276ec0d5155dbacc6728b3403654969c84a3
Pedro Fornaza <pedro.fornaza@gmail.com>
2013-08-03 15:51:09 -0300

    Add Entity and Mapper for Task

---
+++ src/Tasker/Entities/Task.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Tasker\Entities;
+
+class Task
+{
+	protected $id;
+	protected $project;
+	protected $description;
+	protected $status;
+
+	protected $validStatus = array(
+		'ready',
+		'doing',
+		'done'
+	);
+
+	public function setId($id)
+	{
+		if (!is_int($id) && 
+			!is_numeric($id)
+		) {
+			throw new \InvalidArgumentException('The task id must be integer.');
+		}
+
+		$this->id = (int) $id;
+		return $this;
+	}
+
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	public function setProject($project)
+	{
+		if (!is_int($project) &&
+			!is_numeric($project)
+		) {
+			throw new \InvalidArgumentException('The task project must be integer.');
+		}
+
+		$this->project = (int) $project;
+		return $this;
+	}
+
+	public function getProject()
+	{
+		return $this->project;
+	}
+
+	public function setName($name)
+	{
+		$name = trim($name);
+		if ($name == '') {
+			throw new \InvalidArgumentException('The task name cannot be null.');
+		}
+
+		$this->name = $name;
+		return $this;
+	}
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function setDescription($description)
+	{
+		$description = trim($description);
+		if ($description == '') {
+			throw new \InvalidArgumentException('The task description cannot be null.');
+		}
+
+		$this->description = $description;
+		return $this;
+	}
+
+	public function getDescription()
+	{
+		return $this->description;
+	}
+
+	public function setStatus($status)
+	{
+		$status = strtolower($status);
+		if (!in_array($status, $this->validStatus)) {
+			throw new \InvalidArgumentException('The task status must be Ready, Doing or Done.');
+		}
+
+		$this->status = $status;
+		return $this;
+	}
+
+	public function getStatus()
+	{
+		return $this->status;
+	}
+}
---
+++ src/Tasker/Mappers/Task.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Tasker\Mappers;
+
+use Tasker\Entities\Task as Entity;
+
+class Task
+{
+	protected $db;
+
+	public function __construct(\PDO $db)
+	{
+		$this->db = $db;
+	}
+
+    public function save(Entity $task)
+    {
+        if ($task->getId() !== null) {
+            $this->update($task);
+        } else {
+            $this->insert($task);
+        }
+    }
+
+	public function insert(Entity $task)
+    {
+    	$sql = 'INSERT INTO tasks (project, name, description, status) VALUES (:project, :name, :description, :status)';
+    	$stm = $this->db->prepare($sql);
+
+        $stm->bindValue(':project', $task->getProject());
+        $stm->bindValue(':name', $task->getName());
+    	$stm->bindValue(':description', $task->getDescription());
+    	$stm->bindValue(':status', $task->getStatus());
+
+    	if (!$stm->execute()) {
+            throw new \Exception("Something went wrong inserting the task '{$task->getName()}'");
+        }
+
+
+    	$id = $this->db->lastInsertId();
+    	$task->setId($id);
+    }
+
+    public function update(Entity $task)
+    {
+    	$sql = 'UPDATE tasks set name = :name, description = :description, status = :status WHERE id = :id';
+    	$stm = $this->db->prepare($sql);
+
+        $stm->bindValue(':name', $task->getName());
+    	$stm->bindValue(':description', $task->getDescription());
+    	$stm->bindValue(':status', $task->getStatus());
+    	$stm->bindValue(':id', $task->getId());
+
+    	if (!$stm->execute()) {
+    		throw new \Exception("Something went wrong updating the task '{$task->getName()}'");
+    	}
+    }
+
+	public function get($id)
+	{
+		$sql = 'SELECT * FROM tasks WHERE id = :id';
+		$stm = $this->db->prepare($sql);
+		$stm->bindValue(':id', $id);
+
+		if (!$stm->execute()) {
+			throw new \Exception('The task cannot be found.');
+		}
+
+		$result = $stm->fetch();
+		$entity = new Entity;
+		$entity->setId($result['id'])
+               ->setName($result['name'])
+			   ->setDescription($result['description'])
+			   ->setStatus($result['status']);
+
+		return $entity;
+	}
+
+    public function getByProject($project)
+    {
+        $sql = 'SELECT * FROM tasks WHERE project = :project';
+        $stm = $this->db->prepare($sql);
+        $stm->bindValue(':project', $project);
+        $stm->execute();
+
+        $result = $stm->fetchAll();
+
+        $tasks = array();
+
+        foreach ($result as $row) {
+            $entity = new Entity;
+            $entity->setId($row['id'])
+                   ->setName($row['name'])
+                   ->setDescription($row['description'])
+                   ->setStatus($row['status']);
+
+            $tasks[] = clone $entity;
+        }
+
+        return $tasks;
+    }
+
+    public function getAll()
+    {
+        $sql = 'SELECT * FROM tasks';
+        $stm = $this->db->prepare($sql);
+        $stm->execute();
+
+        $result = $stm->fetchAll();
+
+        $tasks = array();
+
+        foreach ($result as $row) {
+            $entity = new Entity;
+            $entity->setId($row['id'])
+                   ->setName($row['name'])
+                   ->setDescription($row['description'])
+                   ->setStatus($row['status']);
+
+            $tasks[] = clone $entity;
+        }
+
+        return $tasks;
+    }
+}
