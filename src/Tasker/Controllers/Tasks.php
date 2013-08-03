82c4161dce767b1543210bd04e211128c6f457e3
Pedro Fornaza <pedro.fornaza@gmail.com>
2013-08-03 15:55:52 -0300

    Add Controller and templates to Task

---
+++ src/Tasker/Controllers/Tasks.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tasker\Controllers;
+
+use Tasker\Entities\Task as TaskEntity;
+use Tasker\Mappers\Task as TaskMapper;
+use Tasker\Mappers\Time as TimeMapper;
+use Tasker\View;
+
+class Tasks
+{
+    protected $db;
+
+    public function __construct(\PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function get($id = null)
+    {
+        $mapper = new TaskMapper($this->db);
+
+        if ($id == null) {
+            echo 'You should specify a Task to search';
+            exit;
+        }
+        
+        $task = $mapper->get($id);
+
+        $taskMapper = new TimeMapper($this->db);
+        $tasks = $taskMapper->getByTask($id);
+
+        $viewName = '../templates/tasks/detail.php';
+        $viewParams = array(
+            'task' => $task,
+            'times'   => $tasks
+        );
+
+        $view = new View($viewName);
+        return $view->render($viewParams);
+    }
+
+    public function post($id = null)
+    {
+        $mapper = new TaskMapper($this->db);
+
+        try {
+            $entity = new TaskEntity();
+            if ($id != null) {
+                $entity = $mapper->get($id);
+            }
+
+            $entity->setProject($_POST['task']['project'])
+                   ->setName($_POST['task']['name'])
+                   ->setDescription($_POST['task']['description'])
+                   ->setStatus($_POST['task']['status']);
+
+            $mapper->save($entity);
+
+            header('Location: /tasks/'.$entity->getId());
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
---
+++ templates/tasks/detail.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en-us">
+<head>
+	<meta charset="utf8" />
+	<title>Tasker - Project Management made easy</title>
+</head>
+<body>
+	<h1>#<?= $params['task']->getId() ?> - <?= $params['task']->getName() ?></h1>
+
+	<o>Description: <?= $params['task']->getDescription() ?></p>
+	<p>Status: <?= ucfirst($params['task']->getStatus()) ?></p>
+
+	<form method="post" action="/times">
+		<input type="hidden" name="time[type]" value="start" />
+		<input type="hidden" name="time[task]" value="<?= $params['task']->getId() ?>" />
+		<button type="submit">Start Time</button>
+	</form>
+
+	<?php if (!empty($params['times'])) : ?>
+	<h2>Times</h2>
+	<ul>
+		<?php foreach ($params['times'] as $time) : ?>
+			<li><?= $time->getStart() ?> <?php if ($time->getEnd() == null) : ?> - <a href="/time/">Close</a><?php endif; ?></li>
+		<?php endforeach ?>
+	</ul>
+	<?php endif ?>
+</body>
+</html>
