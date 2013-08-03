437fb01be157f4f32cf2dc9520b147117824a6f0
Pedro Fornaza <pedro.fornaza@gmail.com>
2013-08-03 15:56:21 -0300

    Add Controller to Time

---
+++ src/Tasker/Controllers/Times.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tasker\Controllers;
+
+use Tasker\Entities\Time as TimeEntity;
+use Tasker\Mappers\Time as TimeMapper;
+use Tasker\View;
+
+class Times
+{
+    protected $db;
+
+    public function __construct(\PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function get($id = null)
+    {
+        echo 'You cannot access a time registry';
+        exit;
+    }
+
+    public function post($id = null)
+    {
+        $mapper = new TimeMapper($this->db);
+
+        try {
+            $datetime = new \Datetime();
+            
+            $entity = new TimeEntity();
+            $entity->setStart($datetime->format('Y-m-d h:i:s'));
+            if ($_POST['time']['type'] == 'end') {
+                $entity = $mapper->get($id);
+                $entity->setEnd($datetime->format('Y-m-d h:i:s'));
+            }
+
+            $entity->setTask($_POST['time']['task']);
+
+            $mapper->save($entity);
+
+            header('Location: /tasks/'.$entity->getTask());
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
